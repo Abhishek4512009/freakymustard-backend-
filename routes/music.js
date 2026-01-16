63710000eb90138e5206666d438931cf65ffa32b
Abhishek4512009 <abhishek.carmel2009@gmail.com>
2026-01-16 16:32:27 +0530

    Update music.js

--- routes/music.js
+++ routes/music.js
@@ -191,7 +191,7 @@ router.post('/download', async (req, res) => {
             '--no-check-certificates',
             '--remote-components', 'ejs:github',
             '--user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            //   '--js-runtimes', 'node' // Attempt to fix JS warning
+            '--js-runtimes', 'node' // Attempt to fix JS warning
         ];
 
         if (fs.existsSync(WRITABLE_COOKIES_PATH)) {
