43a43832c55a0f4b637f297b00f736c8333769ad
Abhishek4512009 <abhishek.carmel2009@gmail.com>
2026-01-16 15:56:04 +0530

    Delete models directory

--- models/Subtitle.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SubtitleSchema = new mongoose.Schema({
-    fileId: { type: String, required: true, index: true }, // The Video File ID
-    content: { type: String, required: true }, // VTT Content
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Subtitle', SubtitleSchema);
